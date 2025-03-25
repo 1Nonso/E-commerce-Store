4fb34cd4025eac108b1502fc63b0a8adcaeaf71f
Othniel <chukchi2345@gmail.com>
2025-03-25 16:01:32 +0100

    Creation of Navigation bar

---
+++ src/Component/Navigations/Navbar.jsx
@@ -0,0 +1,16 @@
+import MenuBar from "../../assets/vector.jpg"
+
+const NavBar = () => {
+  return (
+    <>
+      <section>
+        <div>
+            <img src={MenuBar} alt="" />
+        </div>
+        <div></div>
+      </section>
+    </>
+  );
+};
+
+export default NavBar;
